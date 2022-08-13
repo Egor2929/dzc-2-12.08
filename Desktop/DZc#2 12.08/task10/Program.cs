4be274241db08444bd2e7203c1763e155c722eb8
Egor Еtrofimov <martynenkoa01@gmail.com>
2022-08-13 11:30:38 +0300

    Final version

---
+++ Desktop/DZc#2 12.08/task10/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число : ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a >= 100 & a < 1000)
+{
+    int digit1 = a / 10;
+    int digit2 = digit1 % 10;
+    Console.WriteLine($"Вторая цифра {digit2} ");
+}
+else
+{
+Console.WriteLine("Число имеет больше трех символов");
+}
+
+
+
+
+
+// Console.WriteLine("Введите трехзначное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if (number >= 100 & number < 1000)
+// {
+//     int res = number / 10;
+//     int res1 = res % 10;
+//     Console.WriteLine($"Вторая цифра числа {number} - {res1}");
+// }
+// else
+// {
+//     Console.WriteLine("Введено некорректное число");
+// }
